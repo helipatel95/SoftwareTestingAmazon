69a86c41bf852a4d4e68c85f70ea038c9c7d7782
nisargp0306 <66429558+nisargp0306@users.noreply.github.com>
2020-12-09 12:31:34 -0500

    Nisarg Commited

---
+++ Nisarg/TestCases/src/nisarg_selenium/nisargAmazonBestSellers.java
@@ -0,0 +1,52 @@
+package nisarg_selenium;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.sikuli.script.FindFailed;
+import org.sikuli.script.Pattern;
+import org.sikuli.script.Screen;
+
+public class nisargAmazonBestSellers {
+
+	static WebDriver driver;
+	Screen myScreen;
+	JavascriptExecutor jse;
+	
+	public void invokeBrowser() {
+		
+		System.setProperty("webdriver.chrome.driver", 
+				"C:\\Selenium\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		
+		driver.get("https://www.amazon.ca");		
+		
+	}
+	
+	public void selectBestSellers() {
+		try {
+			myScreen = new Screen();
+			Pattern p1 = new Pattern("C:\\Users\\Nisar\\Desktop\\BestSellersAmazon.PNG");
+			
+			
+			myScreen.click(p1);
+			
+			
+		} catch (FindFailed e) {
+			e.printStackTrace();
+			
+		} 
+	}
+	
+	public static void main(String[] args) {
+		nisargAmazonBestSellers obj = new nisargAmazonBestSellers();
+		obj.invokeBrowser();
+		obj.selectBestSellers();
+		
+	}
+}
